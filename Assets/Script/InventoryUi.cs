0dbf7732397da6c385531ee529007c8ab251e217
mangel119 <miguel.sanabria@uao.edu.co>
2022-04-21 06:16:12 -0500

    Simple Inventory

---
+++ Assets/Script/InventoryUi.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUi : MonoBehaviour
+{
+    public GameObject inventoryUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory")){
+            inventoryUI.SetActive (!inventoryUI.activeSelf);
+        }
+        
+    }
+}
